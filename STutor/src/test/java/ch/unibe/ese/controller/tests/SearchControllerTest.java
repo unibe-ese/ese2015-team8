78e09b582338aed64fbccf21edd8dd6556d59abd
Stefan Jonas <stefan.jonas@students.unibe.ch>
2015-11-11 11:34:09 +0100

    Finished (basic) search controller test

--- STutor/src/test/java/ch/unibe/ese/controller/tests/SearchControllerTest.java
+++ STutor/src/test/java/ch/unibe/ese/controller/tests/SearchControllerTest.java
@@ -3,11 +3,14 @@ package ch.unibe.ese.controller.tests;
 import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
 
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
 
 
 
@@ -15,8 +18,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.test.context.support.WithMockUser;
+
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -27,6 +29,14 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
+
+import ch.unibe.ese.model.Lecture;
+import ch.unibe.ese.model.Student;
+import ch.unibe.ese.model.dao.LectureDao;
+import ch.unibe.ese.model.dao.StudentDao;
+
+import static org.hamcrest.Matchers.*;
+
 
 
 
@@ -42,6 +52,8 @@ public class SearchControllerTest {
 	
 	@Autowired private WebApplicationContext wac;
 	@Autowired private FilterChainProxy springSecurityFilterChain;
+	@Autowired private StudentDao studentDao;
+	@Autowired private LectureDao lectureDao;
 	
 	private MockMvc mockMvc;
 
@@ -53,16 +65,66 @@ public class SearchControllerTest {
 	
 	@Test
 	public void testRequestMapping() throws Exception{
-
-		ModelAndView mav = mockMvc.perform(get("/basicSearch?q=test")).andReturn().getModelAndView();
-						
+		ModelAndView mav = mockMvc.perform(get("/basicSearch?q=")).andReturn().getModelAndView();					
 		assertViewName(mav, "searchResult");
 	}
 	
 	@Test
 	public void testEmptyReults() throws Exception{
+		mockMvc.perform(get("/basicSearch?q="))
+											.andExpect(model().attributeExists("lectures"))
+											.andExpect(model().attribute("lectures", hasSize(0)))
+											.andExpect(model().attributeExists("tutors"))
+											.andExpect(model().attribute("tutors", hasSize(0)));
 		
 	}
+	
+	//test a valid search that returns the created lecture and tutor
+	@Test
+	public void testGettingReults() throws Exception{
+		
+		Student tutor = initializeTutor("tutorForTest");
+		
+		Lecture lecture = initizalieLecture("Introduction to Testing", tutor);
+		
+	
+		//search results are in a list, so let's put our tutor in one for later comparison
+		List<Student> ourTutors = new LinkedList<Student>();
+		ourTutors.add(tutor);
+		
+		mockMvc.perform(get("/basicSearch?q="+lecture.getName()))
+											.andExpect(model().attributeExists("lectures"))
+											.andExpect(model().attribute("lectures", hasSize(1)))
+											.andExpect(model().attribute("tutors", hasSize(1)))
+											
+											//make sure that it is indeed our tutor that has been found
+											
+											.andExpect(model().attribute("tutors", ourTutors));
+		
+	}
+	
+	
+
+
+	private Student initializeTutor(String username){
+		Student tutor = new Student();
+		tutor.setUsername(username);
+		tutor.setId((long) -1);
+		tutor = studentDao.save(tutor);
+		
+		return tutor;
+	}
+	
+	private Lecture initizalieLecture(String name, Student tutor) {
+		Lecture lecture = new Lecture();
+		lecture.setName(name);
+		lecture.setId((long) -1);
+		lecture.setTutor(tutor);
+
+		lecture = lectureDao.save(lecture);
+		return lecture;
+	}
+
 	
 	
 	
